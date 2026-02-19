a1160fdff1fcd848ca5b7e8a621c7bda6eb5b781
Saleh <salehalswat2@gmail.com>
2026-02-19 12:21:18 +0300

    Lab Function Feb 19, 2026

---
+++ Lab_functions.py
@@ -0,0 +1,9 @@
+#lab functions
+
+def patter(number:int):
+    '''This function prints a pattern of numbers based on the input number.'''
+    for i in range(number, 0, -1):
+        for j in range(i):
+            print(i-j, end=' ')
+        print()
+patter(5)
